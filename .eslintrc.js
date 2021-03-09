07557cbb3e9ec3a1da0b5c11103c274c03c847b8
KimYoHwan <radline000@gmail.com>
2021-03-09 18:27:43 +0900

    radline000

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+	root: true,
+	env: {
+		node: true,
+	},
+	extends: [
+		'plugin:vue/essential',
+		'eslint:recommended',
+		'@vue/typescript/recommended',
+		'@vue/prettier',
+		'@vue/prettier/@typescript-eslint',
+	],
+	parserOptions: {
+		ecmaVersion: 2020,
+	},
+	rules: {
+		'no-console': 'off',
+		// "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+		// "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+		'prettier/prettier': [
+			'error',
+			{
+				singleQuote: true,
+				semi: false,
+				useTabs: true,
+				tabWidth: 2,
+				trailingComma: 'all',
+				printWidth: 80,
+				bracketSpacing: true,
+				arrowParens: 'avoid',
+			},
+		],
+	},
+	overrides: [
+		{
+			files: [
+				'**/__tests__/*.{j,t}s?(x)',
+				'**/tests/unit/**/*.spec.{j,t}s?(x)',
+			],
+			env: {
+				jest: true,
+			},
+		},
+	],
+};
